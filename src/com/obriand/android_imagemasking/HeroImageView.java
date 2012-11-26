1d9067529644f3122254cdf7197d474b39cf67ae
Olivier Briand <olivier.briand@orange.com>
2012-11-26 15:37:19 +0100

    OK with mask, shadow and border

--- src/com/obriand/android_imagemasking/HeroImageView.java
+++ src/com/obriand/android_imagemasking/HeroImageView.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
@@ -15,74 +16,66 @@ public class HeroImageView extends ImageView {
 	
     private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     
-    private Bitmap mIcon;
-    private Bitmap mIconMask;
+    private Bitmap mImage;
+    private Bitmap mImageMask;
+    private Bitmap mImageOmbre;
 
 	public HeroImageView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
-		
-        // Prepares the paint that will be used to draw our icon mask. Using
-        // PorterDuff.Mode.DST_IN means the image that will be drawn will
-        // mask the already drawn image.
-        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-        // Let's retrieve all icon pieces as Bitmaps.
-        final Resources res = context.getResources();
-        mIcon = BitmapFactory.decodeResource(res, R.drawable.igadget);
-        //mIconGlossy = BitmapFactory.decodeResource(res, R.drawable.icon_glossy);
-        mIconMask = BitmapFactory.decodeResource(res, R.drawable.vignette_hero_01);
+		init(context);
 	}
 	
     public HeroImageView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
-        // TODO Auto-generated constructor stub
-        
-        // Prepares the paint that will be used to draw our icon mask. Using
-        // PorterDuff.Mode.DST_IN means the image that will be drawn will
-        // mask the already drawn image.
-        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-        // Let's retrieve all icon pieces as Bitmaps.
-        final Resources res = context.getResources();
-        mIcon = BitmapFactory.decodeResource(res, R.drawable.igadget);
-        //mIconGlossy = BitmapFactory.decodeResource(res, R.drawable.icon_glossy);
-        mIconMask = BitmapFactory.decodeResource(res, R.drawable.vignette_hero_01);
+        // TODO Auto-generated constructor stub   
+        init(context);
     }
 
     public HeroImageView(Context context, AttributeSet attrs, int defStyle)
     {
         super(context, attrs, defStyle);
         // TODO Auto-generated constructor stub
-        
-        // Prepares the paint that will be used to draw our icon mask. Using
-        // PorterDuff.Mode.DST_IN means the image that will be drawn will
-        // mask the already drawn image.
+        init(context);
+    }
+    
+    public void init(Context context) {
+
         mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-        // Let's retrieve all icon pieces as Bitmaps.
         final Resources res = context.getResources();
-        mIcon = BitmapFactory.decodeResource(res, R.drawable.igadget);
-        //mIconGlossy = BitmapFactory.decodeResource(res, R.drawable.icon_glossy);
-        mIconMask = BitmapFactory.decodeResource(res, R.drawable.vignette_hero_01);
+        mImageMask = BitmapFactory.decodeResource(res, R.drawable.vignette_hero_02);       
+        mImageOmbre = BitmapFactory.decodeResource(res, R.drawable.ombre_vignette_hero_02);
     }
 	
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.save();
-
-        // Translate the canvas in order to draw the icon in the center of
-        // the view
-        //canvas.translate((getWidth() - mIcon.getWidth()) >> 1, (getHeight() - mIcon.getHeight()) >> 1);
-
-        // We're now ready to drawn our iPhone-like icon :)
-        //canvas.drawBitmap(mIcon, 0, 0, null);
-        //canvas.drawBitmap(mIconGlossy, 0, 0, null);
         
-        Bitmap mIconScaledMask = mIconMask.createScaledBitmap(mIconMask, mIcon.getWidth(), mIcon.getHeight(), true);
+        Bitmap mImageScaledMask = mImageMask.createScaledBitmap(mImageMask, getWidth(), getHeight(), true);
+        Bitmap mtImageScaledMask = replaceColor(mImageScaledMask);
+        canvas.drawBitmap(mtImageScaledMask, 0, 0, mPaint); 
         
-        canvas.drawBitmap(mIconScaledMask, 0, 0, mPaint);
-
+        Bitmap mImageScaledOmbre = mImageOmbre.createScaledBitmap(mImageOmbre, getWidth(), getHeight(), true);        
+        canvas.drawBitmap(mImageScaledOmbre, 0, 0, null);
+        
         canvas.restore();
+    }
+    
+	private Bitmap replaceColor(Bitmap bm) {
+		Bitmap newBm = bm;
+		int[] allpixels = new int[bm.getHeight() * bm.getWidth()];
+		bm.getPixels(allpixels, 0, bm.getWidth(), 0, 0, bm.getWidth(),
+				bm.getHeight());
+		for (int i = 0; i < newBm.getHeight() * newBm.getWidth(); i++) {
+			if (allpixels[i] == Color.BLACK)
+				allpixels[i] = Color.TRANSPARENT;
+		}
+		newBm.setPixels(allpixels, 0, newBm.getWidth(), 0, 0, newBm.getWidth(),
+				newBm.getHeight());
+
+		return newBm;
     }
 	
 }
--- src/com/obriand/android_imagemasking/MainActivity.java
+++ src/com/obriand/android_imagemasking/MainActivity.java
@@ -4,20 +4,7 @@ import com.obriand.android_imagemasking.R;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -44,51 +31,5 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-	
-    // Class IconView from View
-	private class IconView extends View {
-    	 
-        private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
- 
-        private Bitmap mIcon;
-        private Bitmap mIconGlossy;
-        private Bitmap mIconMask;
- 
-        public IconView(Context context) {
-            super(context);
- 
-            // Prepares the paint that will be used to draw our icon mask. Using
-            // PorterDuff.Mode.DST_IN means the image that will be drawn will
-            // mask the already drawn image.
-            mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
- 
-            // Let's retrieve all icon pieces as Bitmaps.
-            final Resources res = context.getResources();
-            mIcon = BitmapFactory.decodeResource(res, R.drawable.igadget);
-            //mIconGlossy = BitmapFactory.decodeResource(res, R.drawable.icon_glossy);
-            mIconMask = BitmapFactory.decodeResource(res, R.drawable.vignette_hero_02);
-        }
- 
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            canvas.save();
- 
-            // Translate the canvas in order to draw the icon in the center of
-            // the view
-            canvas.translate((getWidth() - mIcon.getWidth()) >> 1, (getHeight() - mIcon.getHeight()) >> 1);
- 
-            // We're now ready to drawn our iPhone-like icon :)
-            //canvas.drawBitmap(mIcon, 0, 0, null);
-            //canvas.drawBitmap(mIconGlossy, 0, 0, null);
-            
-            //Bitmap mIconScaledMask = mIconMask.createScaledBitmap(mIconMask, 200, 200, true);
-            
-            canvas.drawBitmap(mIconMask, 0, 0, mPaint);
- 
-            canvas.restore();
-        }
- 
-    }
 	
 }
